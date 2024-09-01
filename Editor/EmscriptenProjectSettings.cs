b309120d8de5a32d7aaa9f380b67591b247f4f7c
gilzoide <gilzoide@gmail.com>
2024-09-01 20:01:43 -0300

    Rename assembly to Gilzoide.WebGLEmscriptenSettings.Editor

--- Editor/EmscriptenProjectSettings.cs
+++ Editor/EmscriptenProjectSettings.cs
@@ -1,7 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace Gilzoide.EmscriptenSettings.Editor
+namespace Gilzoide.WebGLEmscriptenSettings.Editor
 {
     public class EmscriptenProjectSettings
     {
