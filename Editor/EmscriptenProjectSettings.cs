3df9a0387dddf5a988104459ca78981707aec5ab
gilzoide <gilzoide@gmail.com>
2024-09-01 20:00:51 -0300

    Initial commit

---
+++ Editor/EmscriptenProjectSettings.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Gilzoide.EmscriptenSettings.Editor
+{
+    public class EmscriptenProjectSettings
+    {
+        private static GUIContent _emscriptenArgsLabel = new GUIContent("Emscripten Args", "Arguments passed to the emscripten compiler during WebGL builds");
+
+        [SettingsProvider]
+        public static SettingsProvider CreateEmscriptenProjectSettings()
+        {
+            return new SettingsProvider("Project/WebGL Emscripten Settings", SettingsScope.Project)
+            {
+                guiHandler = searchContext =>
+                {
+                    PlayerSettings.WebGL.emscriptenArgs = EditorGUILayout.TextField(_emscriptenArgsLabel, PlayerSettings.WebGL.emscriptenArgs);
+                },
+            };
+        }
+    }
+}
